cd4c85fa304a5151035e36850114992f17a99000
vviktorian <mygithubmail.wn@gmail.com>
2021-09-15 20:40:57 +0200

    js before validation

--- main.js
+++ main.js
@@ -3,17 +3,30 @@ window.addEventListener('DOMContentLoaded', function(){
     var pplCountInput = document.querySelector('.pplCount');
     var tipOptionsArea= document.querySelector('.tip-options');
     var tipOptions = tipOptionsArea.querySelectorAll('.tip-option');
+    let total = document.querySelector('.total-ready');
+    let tip = document.querySelector('.tip-ready');
+    
     let tipPrcnt = 0;
     tipOptions.forEach(function(option){
         option.addEventListener('click', function(){
             var prcntVal = option.getAttribute('val'); 
-            tipPrcnt = prcntVal;
+            tipPrcnt = parseInt(prcntVal);
         })
     })
     pplCountInput.addEventListener('input', function(){
-        var billValue = billInput.value;
-        var pplCount = pplCountInput.value;
+        var billValue = parseInt(billInput.value);
+        var pplCount = parseInt(pplCountInput.value);
 
-        console.log(billValue + pplCount);
+        var tipValue = billValue * (tipPrcnt / 100);
+        var tipPPerson = parseFloat(tipValue / pplCount);
+        var totalPPerson = billValue / pplCount; 
+        
+        if(isNaN(totalPPerson) == false){
+            total.innerHTML = totalPPerson.toFixed(2);
+        }
+        
+        if(isNaN(tipPPerson) == false & tipPPerson != 0){
+            tip.innerHTML = tipPPerson.toFixed(2);
+        }
     })
 })
