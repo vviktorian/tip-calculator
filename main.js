7f37adfd99f3369cfe96e96f4190baf0909b490a
vviktorian <mygithubmail.wn@gmail.com>
2021-09-20 16:39:25 +0200

    working on tip options

--- main.js
+++ main.js
@@ -1,32 +1,54 @@
-window.addEventListener('DOMContentLoaded', function(){
-    var billInput = document.querySelector('.billCount');
-    var pplCountInput = document.querySelector('.pplCount');
-    var tipOptionsArea= document.querySelector('.tip-options');
-    var tipOptions = tipOptionsArea.querySelectorAll('.tip-option');
-    let total = document.querySelector('.total-ready');
-    let tip = document.querySelector('.tip-ready');
-    
-    let tipPrcnt = 0;
-    tipOptions.forEach(function(option){
-        option.addEventListener('click', function(){
-            var prcntVal = option.getAttribute('val'); 
-            tipPrcnt = parseInt(prcntVal);
-        })
-    })
-    pplCountInput.addEventListener('input', function(){
-        var billValue = parseInt(billInput.value);
-        var pplCount = parseInt(pplCountInput.value);
+window.addEventListener("DOMContentLoaded", function () {
+  var inputs = document.querySelectorAll("input");
+  var tipOptions = document.querySelectorAll(".tip-option");
+  var billInput = document.querySelector(".billInput");
+  var tipInput = document.querySelector(".tip-input");
+  var pplInput = document.querySelector(".pplInput");
+  var totalOutput = document.querySelector(".total-ready");
+  var tipOutput = document.querySelector(".tip-ready");
+  // var warning = document.querySelector('.warning');
 
-        var tipValue = billValue * (tipPrcnt / 100);
-        var tipPPerson = parseFloat(tipValue / pplCount);
-        var totalPPerson = billValue / pplCount; 
-        
-        if(isNaN(totalPPerson) == false){
-            total.innerHTML = totalPPerson.toFixed(2);
-        }
-        
-        if(isNaN(tipPPerson) == false & tipPPerson != 0){
-            tip.innerHTML = tipPPerson.toFixed(2);
-        }
-    })
-})
+  let tip = 0;
+  tipOptions.forEach(function (option) {
+    option.addEventListener("click", function () {
+      var prcntVal = option.getAttribute("val");
+      tip = parseInt(prcntVal);
+      tipInput.value = 0;
+    });
+  });
+
+  inputs.forEach(function (input) {
+    input.addEventListener("input", function () {
+      var bill = parseInt(billInput.value);
+      var customTip = parseInt(tipInput.value);
+      var ppl = parseInt(pplInput.value);
+      var total = bill / ppl;
+      var tipValue = bill * (tip / 100);
+      var customTipValue = bill * (customTip / 100);
+      var tipPPerson = parseFloat(tipValue / ppl);
+      var customTipPPerson = parseFloat(customTipValue / ppl);
+
+      if (
+        ppl > 0 &&
+        isNaN(total) == false &&
+        total != 0 &&
+        bill != 0 &&
+        bill > 0
+      ) {
+        totalOutput.innerHTML = total.toFixed(2);
+      } else {
+        totalOutput.innerHTML = " ";
+      }
+
+      if (tip == 0 || tip < 0 || isNaN(tip) == true) {
+        tipOutput.innerHTML = customTipPPerson.toFixed(2);
+      }
+      else if(customTip == 0 || customTip < 0 || isNaN(customTip) == true){
+          tipOutput.innerHTML = tipPPerson.toFixed(2);
+      }
+      else{
+          tipOutput.innerHTML = " ";
+      }
+    });
+  });
+});
